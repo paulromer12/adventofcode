b4e5a7c50dd7b230fd4edc340db35bf0b2c17d6e
Paul <paulromer2036@gmail.com>
2021-12-13 14:15:55 -0700

    Finished day 1 of advent 2021

--- day1.py
+++ day1.py
@@ -10,10 +10,13 @@ for line in data:
     list.append(int(line.strip('\n')))
 
 counter = 0
+counter1 = 0
 
-for i in range(0, 2000):
-    if list[i] > list[(i - 1)]:
+for i, num in enumerate(list):
+    if i >= 0 and list[i] > list[(i - 1)]:
         counter += 1
-        print(counter)
+    if i >= 3 and list[i]+list[i-1]+list[i-2] > list[i-1]+list[i-2]+list[i-3]:
+        counter1 += 1
 
 print(counter)
+print(counter1)
