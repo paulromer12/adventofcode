4288a7e1a802255bb8fa52e9a93bc94fee9e9e8d
Paul <paulromer2036@gmail.com>
2021-12-18 17:08:40 -0700

    Sill working on day3. First item put in a list and compared zeros to ones"

--- day3.py
+++ day3.py
@@ -8,9 +8,23 @@ for line in data:
     list.append(line.strip('\n'))
 
 #each has 12 bits
-
+#create a list with the 1st charater of each item
 for i in list:
-    bits1.append(list[0])
+    bits1.append(i[0])
+
+zero = 0
+one = 0
+for i in bits1:
+    if int(i) == 0:
+        zero += 1
+    if int(i) == 1:
+        one += 1
+
+if zero > one:
+    print("There are more zeros. " + str(zero))
+else:
+    print("There are more ones. " + str(one))
+
 
 gamma_rate = 0
 epsilon_rate = 0
@@ -18,5 +32,3 @@ power_consumption = gamma_rate * epsilon_rate
 
 binary = '10110'
 integer = int(binary, 2)
-
-print(bits1)
